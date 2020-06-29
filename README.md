8efb75a4f6ba357e5c3bf5ef0203ac1a9a7909da
Pop0097 <dhruvrawat09@gmail.com>
2020-06-29 12:06:20 -0400

    Creates training data

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ I made this program to learn more about audio processing, TensorFlow, and machin
 # How it works
 
 1. The user will record themselves saying "Hey Pop" five times. Afterwards, the progarm will process the audio and use TensorFlow to create a keras model to recognize for the user's voice.
-2. Once the model is made, the user can say "Hey Pop" into their microphone and the program will identify whether it is the original user or now. 
+2. Once the model is made, the user can say "Hey Pop" into their microphone and the program will identify whether it is the original user or now.
 
 # Important information
 
@@ -17,6 +17,14 @@ I made this program to learn more about audio processing, TensorFlow, and machin
 
 - sounddevice
 - write
+- librosa
+- numba==0.48
+- matplotlib
+- numpy
+- glob
+- TensorFlow
+- keras
+- pillow
 
 ### Python version used
 
--- speech-recognition.py
+++ speech-recognition.py
@@ -1,7 +1,19 @@
 # imports all required modules
-# packages for recording audio
+# modules for recording audio
 import sounddevice as sd
 from scipy.io.wavfile import write
+
+# modules for creating .png files
+import librosa
+import librosa.display
+import matplotlib.pyplot as plt
+import numpy as np
+import glob
+
+# modules for transforming image
+from tensorflow.python.keras.preprocessing.image import load_img
+from keras.preprocessing.image import img_to_array
+from keras.preprocessing.image import ImageDataGenerator
 
 
 # records five audio samples that will be used for the machine learning
@@ -20,7 +32,81 @@ for i in range(5):
     sd.wait() # line tells program to wait until the three seconds are up
     print("Done recording " + str(i+1) + "/5.\n\n")
 
-    audio_path = "audio/audio_input_" + str(i+1) + ".wav" # initializes path of the audio file
+    audio_path = "audio/input-" + str(i+1) + ".wav" # initializes path of the audio file
     write(audio_path, fs, recording) # saves audio input as a WAV audio file in the audio/ directory
+
+# creates training data
+
+def convert_audio_to_jpg(file_name, save_name): # this function creates the .png
+    save_path = "images/"
+    plt.interactive(False) # image will not be interactive
+    clip, sample_rate = librosa.load(file_name, sr=None) # loads file for processing with librosa library
+    fig = plt.figure(figsize=[0.72, 0.72]) # defines figure size
+    # tells program not to add axes to the diagrams
+    ax = fig.add_subplot(111)
+    ax.axes.get_xaxis().set_visible(False)
+    ax.axes.get_yaxis().set_visible(False)
+    ax.set_frame_on(False)
+    S = librosa.feature.melspectrogram(y=clip, sr=sample_rate) # converts WAV file to spectrogram
+    librosa.display.specshow(librosa.power_to_db(S, ref=np.max)) # specifies the characteristics of the spectrogram
+    save_name = save_path + save_name + ".png" # creates path for the .png
+    plt.savefig(save_name, dpi=400, bbox_inches="tight", pad_inches=0) # creates .png files
+    # closes plots and figures
+    plt.close()
+    fig.clf()
+    plt.close(fig)
+    plt.close("all")
+    del file_name, save_name, clip, sample_rate, fig, ax, S # deletes objects
+
+def create_more_training_data(original_file_name):
+    img = load_img(original_file_name) # loads image that will be transformed
+    # convert to numpy array
+    data = img_to_array(img)
+    # expand dimension to one sample
+    samples = np.expand_dims(data, 0)
+    # create image data augmentation generator
+    generate_training_images = ImageDataGenerator(
+        width_shift_range=[-10, 10],
+        brightness_range=[0.6, 1.3],
+        fill_mode="constant"  # should make images fill with black when translating
+    )
+    # prepare iterator
+    it = generate_training_images.flow(samples, batch_size=1)
+    # creates 20 new images
+    for i in range(20):
+        plt.interactive(False)
+        batch = it.next()  # transforms image
+        image = batch[0].astype("uint8")  # gets the image and converts it to an unsigned integer
+        fig = plt.figure(figsize=[0.72, 0.72])  # creates a figure
+        plt.imshow(image)  # shows the transformed image
+        # does not display axes
+        ax = fig.add_subplot(111)
+        ax.axes.get_xaxis().set_visible(False)
+        ax.axes.get_yaxis().set_visible(False)
+        ax.set_frame_on(False)
+        save_path = "training-images/transformed-image-" + str(i) + ".png"  # creates file path
+        plt.savefig(save_path, dpi=400, bbox_inches="tight", pad_inches=0) # saves .png of transformed image
+        # closes plots and figures
+        plt.close()
+        fig.clf()
+        plt.close(fig)
+        plt.close("all")
+        del save_path, fig, ax # deletes objects
+
+
+audio_folder = "audio/" + "*.wav" # accesses folder with all audio files
+# loop converts all images to .png and creates more training data
+for file in glob.iglob(audio_folder):
+    # 2 step process: 1. splits file name to get name of .wav file. 2. Splits name of .wav file to remove the .wav
+    img_name = file.split("/")[-1].split(".")[0]
+    # print(file + "; " + img_name)
+    convert_audio_to_jpg(file, img_name) # calls function to create .png
+
+image_folder = "images/" + "*.png" # accesses folder with all .png files
+# loop transforms all .png
+for file in glob.iglob(image_folder):
+    create_more_training_data(file) # calls function to create more images
+
+
 
 print("Thank you for using this program!")
